88c17d0a9bf1e432ef0702ad0cd27275a19e8611
Ben Redd <bentheredd13@gmail.com>
2021-10-09 15:01:50 -0700

    Write a general plan for solving the exercise
    Use comments in order to write out a step by step plan for completing
    the exercise. Make sure there is a solid understanding of the
    expectations, and plan to meet those expectations in an efficient way.

---
+++ solution_02/price_calculator.js
@@ -0,0 +1,49 @@
+//what is the problem?
+/*
+ The task is to build a program which when executed would ask the user to list all the items purchased in any order. Once the user has listed all the items, print the total cost. You need to build a solution for the given items and you do not need to worry about items which would be added in future. 
+ */
+
+//  We are looking for you to demonstrate your knowledge related to common software practices to include reusability, portability and encapsulation - to name a few.
+
+//How Will I solve the problem?
+
+//****** Part 1 ******
+//Store data of this weeks pricing table for the local grocery store
+//pricing table
+// Item     Unit price        Sale price
+// --------------------------------------
+// Milk      $3.97            2 for $5.00
+// Bread     $2.17            3 for $6.00
+// Banana    $0.99
+// Apple     $0.89
+
+//****** Part 2 ******
+//prompt the user for an input of the items available at the grocery store
+//be sure to specify parameters for the user input such as commas or spaces between words
+
+//get user input and run a test to ensure that it meets specified parameters
+//convert input into usable data, e.g. an array
+
+//****** Part 3 ******
+//write a function or method that can take the user input, and the store pricing table and then calculate the total cost
+
+//****** Part 4 ******
+//print the total cost in a format similar to what is shown below:
+// Item     Quantity      Price
+// --------------------------------------
+// Milk      3            $8.97
+// Bread     4            $8.17
+// Apple     1            $0.89
+// Banana    1            $0.99
+
+// Total price : $19.02
+// You saved $3.45 today.
+
+//****** Part 5 ******
+//think of various things that could potentially break the code I've written, and write tests to ensure everything is ////working properly
+
+//****** Part 6 ******
+//refactor code to ensure readability and efficiency
+
+//****** Part 7 ******
+//Test again
