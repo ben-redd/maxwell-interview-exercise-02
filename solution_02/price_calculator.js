fbd9b5859b7e48c4d4f9bd419634631892599a29
Ben Redd <bentheredd13@gmail.com>
2021-10-11 14:12:14 -0700

    Seperate pricingTable data from userPurchaseData
    Instead of defining data for the store pricingTable, and user purchase
    data, such as quantity, totalPrice, and totalSavings in the same object
    create a separate object to hold userPurchaseData. Update functions that
    use and modify this data.

--- solution_02/price_calculator.js
+++ solution_02/price_calculator.js
@@ -10,6 +10,7 @@ const prompt = require('prompt-sync')({ sigint: true });
 
 class PriceCalculator {
   constructor() {
+    //current store pricing table
     this.pricingTable = {
       milk: {
         unitPrice: 3.97,
@@ -17,9 +18,6 @@ class PriceCalculator {
         saleUnitNum: 2,
         salePrice: 5.0,
         saleSavings: 2.94,
-        quantity: 0,
-        totalPrice: 0,
-        totalSaved: 0,
       },
       bread: {
         unitPrice: 2.17,
@@ -27,9 +25,6 @@ class PriceCalculator {
         saleUnitNum: 3,
         salePrice: 6.0,
         saleSavings: 0.51,
-        quantity: 0,
-        totalPrice: 0,
-        totalSaved: 0,
       },
       banana: {
         unitPrice: 0.99,
@@ -37,9 +32,6 @@ class PriceCalculator {
         saleUnitNum: undefined,
         salePrice: undefined,
         saleSavings: undefined,
-        quantity: 0,
-        totalPrice: 0,
-        totalSaved: 0,
       },
       apple: {
         unitPrice: 0.89,
@@ -47,23 +39,28 @@ class PriceCalculator {
         saleUnitNum: undefined,
         salePrice: undefined,
         saleSavings: undefined,
-        quantity: 0,
-        totalPrice: 0,
-        totalSaved: 0,
       },
     };
+    //user purchase Data that will contain which items the user buys, quantity of items, total price of each type, and total saved for sale types.
+    this.userPurchaseData = {};
   }
 
   //prompt user for input, then modify and check. Returns a boolean based on the validity of the input
   getUserInput() {
-    let userInput = prompt(
-      'Based on the current store inventory of: Milk, Bread, Banana, and Apple. Please enter all of the items you wish to purchase, separated by commas: ',
+    //separate console.logs from prompt because of newline error with prompt-sync package
+    console.log(
+      '\nBased on the current store inventory of: Milk, Bread, Banana, and Apple.',
     );
+    console.log(
+      'Please enter all of the items you wish to purchase, separated by commas. ',
+    );
+    console.log('e.g. Your input: milk, milk, bread, banana\n');
+    let userInput = prompt('Your input: ');
     console.log(`your input: ${userInput}`);
     //convert userInput into an array. Removes white space and converts to lowercase as well
     let userInputArray = this.convertStringToArray(userInput);
     if (this.isInputValid(userInputArray)) {
-      this.updateQuantity(userInputArray);
+      this.updateUserPurchaseData(userInputArray);
       return true;
     }
     return false;
@@ -85,10 +82,18 @@ class PriceCalculator {
     }
     return true;
   }
-  updateQuantity(inputArr) {
+  updateUserPurchaseData(inputArr) {
     for (let i = 0; i < inputArr.length; i++) {
       if (this.pricingTable.hasOwnProperty(inputArr[i])) {
-        this.pricingTable[inputArr[i]].quantity++;
+        if (this.userPurchaseData.hasOwnProperty(inputArr[i])) {
+          this.userPurchaseData[inputArr[i]].quantity++;
+        } else {
+          this.userPurchaseData[inputArr[i]] = {
+            quantity: 1,
+            totalPrice: 0,
+            totalSaved: 0,
+          };
+        }
       }
     }
   }
@@ -96,48 +101,59 @@ class PriceCalculator {
     let total = 0;
     let amountSaved = 0;
     let dataDisplayArray = [];
-    for (let key in this.pricingTable) {
-      //destructure this.pricingTable[key]
+    for (let key in this.userPurchaseData) {
+      //destructure this.pricingTable[key] and this.userPurchaseData[key]
       let {
-        quantity,
         isOnSale,
-        totalPrice,
         unitPrice,
         saleUnitNum,
         salePrice,
         saleSavings,
-        totalSaved,
       } = this.pricingTable[key];
+      let { quantity } = this.userPurchaseData[key];
       if (quantity > 0) {
         //calculation if item is on sale
         if (isOnSale === true) {
           let saleUnits = Math.floor(quantity / saleUnitNum);
-          totalPrice = Number(
+          this.userPurchaseData[key].totalPrice += Number(
             (saleUnits * salePrice +
               (quantity % saleUnitNum) * unitPrice).toFixed(2),
           );
-          totalSaved = Number((saleUnits * saleSavings).toFixed(2));
-          total += totalPrice;
+          this.userPurchaseData[key].totalSaved = Number(
+            (saleUnits * saleSavings).toFixed(2),
+          );
+          total += this.userPurchaseData[key].totalPrice;
           total = Number(total.toFixed(2));
-          amountSaved += totalSaved;
+          amountSaved += this.userPurchaseData[key].totalSaved;
         } else {
           //normal price calculation if item is not on sale
-          totalPrice = Number((quantity * unitPrice).toFixed(2));
-          total += totalPrice;
+          this.userPurchaseData[key].totalPrice += Number(
+            (quantity * unitPrice).toFixed(2),
+          );
+          total += this.userPurchaseData[key].totalPrice;
           total = Number(total.toFixed(2));
         }
 
         dataDisplayArray.push({
           Item: key,
           Quantity: quantity,
-          Price: `$${totalPrice}`,
+          Price: `$${this.userPurchaseData[key].totalPrice}`,
+          Savings: `$${this.userPurchaseData[key].totalSaved}`,
         });
       }
     }
+    console.log(Object.entries(this.pricingTable));
+    console.log(Object.entries(this.userPurchaseData));
     console.table(dataDisplayArray);
     console.log(`Total price : $${total}`);
     console.log(`Total Savings : $${amountSaved}`);
   }
+
+  //   printPurchaseData(){
+  //       const dataTable = [
+  //           ['Item', 'Quantity', 'Price', 'Savings']
+  //       ]
+  //   }
 }
 
 //execution
@@ -147,11 +163,11 @@ if (purchase.getUserInput()) {
 }
 
 /* *** REFACTOR PLAN ***
--Seperate data structures of pricing table and user input
+-[X]Seperate data structures of pricing table and user input
 
--combine functions into getUserInput
+-[X]combine functions into getUserInput
 
--clean up calculate total cost and write a seperate function to display data
+-[x]clean up calculate total cost and write a seperate function to display data
 
--fix prompt sync re-prompt issue on new lines
+-[]fix prompt sync re-prompt issue on new lines
 */
